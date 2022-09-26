19d454eb53adc01e6df8bb0c4569ca4c5ab7b9b3
bryan <bryan.barra04@inacapmail.cl>
2022-09-26 15:31:23 -0300

    se guardo app1

--- firstApp/views.py
+++ firstApp/views.py
@@ -13,7 +13,7 @@ def displayDatetime(request):
 
 
 def informacion(request):
-    return HttpResponse("<p> información es un conjunto organizado de datos relevantes para uno o más sujetos que extraen de él un conocimiento</p>")
+    return HttpResponse("<p> información es un conjunto organizado de datos relevantes para uno o más sujetos que extraen de él un conocimiento</p><br></br><a>española de telecomunicaciones, con sede central en Madrid. Es la cuarta compañía de telecomunicaciones más importante de Europa y la</a><br></br><div>Países Bajos es miembro de la Unión Europea. Su capital es Ámsterdam sin</div><br></br><body>La más fácil es la de irnos a las propiedades del sistema en el que veremos información como la CPU, la RAM y la versión de Windows</body><br>/<br><strong>Un sistema (del latín systēma, y este del griego σύστημα sýstēma 'reunión</strong>")
 
 
 # Create your views here.
